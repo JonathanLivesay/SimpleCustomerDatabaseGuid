56409f5169c9e7141eb13bfb2497e7dc97e7ac87
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-21 16:43:29 -0500

    Added Domain Test Project and added a test that is neither passing nor not passing

---
+++ SimpleCustomerDatabase.Domain.Tests/When_A_Customer_Logs_In.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SimpleCustomerDatabase.Domain.Tests
+{
+    [TestFixture]
+    public class When_A_Customer_Logs_In
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void When_A_Customer_Logs_In_Here()
+        {
+
+        }
+    }
+}
