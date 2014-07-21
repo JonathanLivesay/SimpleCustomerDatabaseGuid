b95c29499c078ad75f5b4b678ebfd635a536205c
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-21 14:56:34 -0500

    Initial Commit

---
+++ SimpleCustomerDatabase.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleCustomerDatabase.Web.MvcApplication" Language="C#" %>
---
+++ SimpleCustomerDatabase.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleCustomerDatabase.Web.Startup))]
+namespace SimpleCustomerDatabase.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
