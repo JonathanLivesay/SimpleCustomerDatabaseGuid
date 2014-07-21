aac667db8e9925ed4e75cc956786e02bf2509a03
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-21 16:51:06 -0500

    Installed Foundation 5

--- SimpleCustomerDatabase.Web/App_Start/BundleConfig.cs
+++ SimpleCustomerDatabase.Web/App_Start/BundleConfig.cs
@@ -1,4 +1,4 @@
-﻿using System.Web;
+using System.Web;
 using System.Web.Optimization;
 
 namespace SimpleCustomerDatabase.Web
@@ -19,17 +19,13 @@ namespace SimpleCustomerDatabase.Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+           "~/Content/site.css"));
 
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            #region Foundation Bundles
+
+            bundles.Add(Foundation.Scripts());
+            #endregion
         }
     }
 }
