bd78e74af0b9773a4261e92f2ff76ef562ac9d2d
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-22 15:30:02 -0500

    Trying to get Autofac to work so I can have the same customer across all other items

---
+++ SimpleCustomerDatabase.Web/App_Start/ResolverConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace SimpleCustomerDatabase.Web.App_Start
+{
+    public class ResolverConfig
+    {
+        
+
+        
+    }
+}
