15c88bfed95386d808bebc697ba7b766270b00d5
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-22 15:53:58 -0500

    Added the resolverconfig to register in the global.aspx file

--- SimpleCustomerDatabase.Web/Global.asax.cs
+++ SimpleCustomerDatabase.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleCustomerDatabase.Web.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,7 @@ namespace SimpleCustomerDatabase.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ResolverConfig.Register();
         }
     }
 }
