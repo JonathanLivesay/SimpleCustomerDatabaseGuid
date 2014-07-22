caf5c713e534f56f6ded21cd9eb5763b9156e221
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-22 15:51:34 -0500

    Setup Autofac on project and also setup information in resolver config

--- SimpleCustomerDatabase.Web/App_Start/ResolverConfig.cs
+++ SimpleCustomerDatabase.Web/App_Start/ResolverConfig.cs
@@ -4,14 +4,48 @@ using System.Linq;
 using System.Web;
 using Autofac;
 using Autofac.Integration.Mvc;
+using System.Web.Mvc;
+using System.Linq;
+using SimpleCustomerDatabase.Web.Controllers;
+using SimpleCustomerDatabase.Domain;
 
 
 namespace SimpleCustomerDatabase.Web.App_Start
 {
     public class ResolverConfig
     {
-        
+        public static SimpleCustomerDatabase.Domain.Customer instance;
 
+        public static void Register()
+        {
+
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(ResolverConfig).Assembly);
+            builder.Register<SimpleCustomerDatabase.Domain.Customer>(c => instance);
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+        }
+
+        private static void CreateDummyCustomer()
+        {
+
+            instance = new Customer()
+                {
+                    FirstName = "James",
+                    LastName = "Bond",
+                    Email = "James.Bond@customer.com",
+                    PhoneNumber = "214.555.1212",
+                    CompanyName = "Company Work",
+                    CompanyStreet1 = "2525 Alta Vista Dr.",
+                    CompanyStreet2 =  "Suite 400",
+                    CompanyCity = "Addison",
+                    CompanyState = "TX",
+                    CompanyPostalCode = "75087",
+                };
+
+        }
+        
         
     }
 }
