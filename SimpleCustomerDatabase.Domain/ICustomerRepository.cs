d0bfc201c2a0515edf05414158cabc5b16c87780
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-22 10:32:00 -0500

    Added Interface for customers and adding customers to the database

---
+++ SimpleCustomerDatabase.Domain/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCustomerDatabase.Domain
+{
+    interface ICustomerRepository
+    {
+        void AddCustomer(Customer customer);
+
+        void DeleteCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        void ReadCustomer(Customer customer);
+
+    }
+}
