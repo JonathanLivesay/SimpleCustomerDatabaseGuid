1358faceed2ed5cae406912abd44e316a4f15539
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-23 14:32:50 -0500

    Added the class FindCustomerByLastName

---
+++ SimpleCustomerDatabase.Web/Persistence/FindCustomerByLastName.cs
@@ -0,0 +1,19 @@
+﻿using SimpleCustomerDatabase.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Highway.Data;
+
+namespace SimpleCustomerDatabase.Web.Persistence
+{
+    public class FindCustomerByLastName : Query<Customer>
+    {
+        public FindCustomerByLastName(string lastName)
+        {
+            ContextQuery = c => c.AsQueryable<Customer>()
+                .Where(e => e.LastName == lastName);
+        }
+
+    }
+}
