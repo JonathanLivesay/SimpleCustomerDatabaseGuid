3a51d564be95f0e5e2e21224c91acf8361f70147
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-24 10:35:03 -0500

    Figured out how to do validation on my forms setting up the project so that it will run that way

--- SimpleCustomerDatabase.Domain/Customer.cs
+++ SimpleCustomerDatabase.Domain/Customer.cs
@@ -8,14 +8,20 @@ namespace SimpleCustomerDatabase.Domain
 {
     public class Customer
     {
+
         public int Id { get; set; } 
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
         public string CompanyName { get; set; }
 
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
+        [Required]
+        [PhoneNumber]
         public string PhoneNumber { get; set; }
 
         public string CompanyStreet1 { get; set; }
