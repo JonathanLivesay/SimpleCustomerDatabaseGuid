378882a679d9e433717c846eebd32642e45011ca
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-24 10:03:47 -0500

    creating files and stubbing out items for testing the web app

---
+++ SimpleCustomerDatabase.Web.Tests/PersistenceTests/When_A_Customer_Is_Created.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SimpleCustomerDatabase.Web.Tests
+{
+    [TestFixture]
+    public class When_A_Customer_Is_Created
+    {
+        [SetUp]
+        private void SetUp()
+        {
+
+        }
+
+        [TearDown]
+        private void TearDown()
+        {
+
+        }
+
+        [Test]
+        public void When_A_Customer_Is_Created_They_Should_Be_Added_To_the_Customers_Model_List()
+        {
+
+        }
+    }
+}
---
+++ SimpleCustomerDatabase.Web.Tests/PersistenceTests/When_A_Customer_Is_Updated.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCustomerDatabase.Web.Tests
+{
+    [TestFixture]
+    class When_A_Customer_Is_Updated
+    {
+
+        [SetUp]
+        private void SetUp()
+        {
+            
+        }
+
+        [TearDown]
+        private void TearDown()
+        {
+            
+        }
+
+        [Test]
+        private void The_Customers_List_Should_Have_The_Same_Number_Of_Items_As_Before()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
