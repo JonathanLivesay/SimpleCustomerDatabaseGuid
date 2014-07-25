3ab60599df26c643edb9fa1a9d98313ee5654a48
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-07-25 11:19:57 -0500

    fixed the menu bar when screen size is set to small, and adding tests for the model

---
+++ SimpleCustomerDatabase.Web.Tests/ControllerTests/CustomerControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using SimpleCustomerDatabase.Web.Controllers;
+using SimpleCustomerDatabase.Domain;
+using NUnit.Framework;
+
+
+namespace SimpleCustomerDatabase.Web.Tests.ControllerTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+
+        private ViewResult viewResult;
+        private Customer customer1;
+        private Customer customer2;
+        private CustomerController customerController;
+        private List<Customer> customers;
+
+        [SetUp]
+        public void SetUp()
+        {
+            customerController = new CustomerController(customer1);
+
+            viewResult = customerController.Index() as ViewResult;
+        }
+
+        [Test]
+        public void When_A_Customer_Logs_In_They_Should_Go_To_The_Index_View()
+        {
+            ViewResult result = customerController.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("", result.ViewName);
+        }
+
+        [Test]
+        public void When_No_Customer_Is_In_The_Model_Then_View_Result_Should_Be_The_First_Page()
+        {
+            Customer customer = null;
+            CustomerController controller = new CustomerController(customer);
+            
+            var result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+            //Assert.AreEqual("First", result);
+        }
+
+
+    }
+}
---
+++ SimpleCustomerDatabase.Web.Tests/ModelTests/CustomerModelTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleCustomerDatabase.Web.Models;
+using SimpleCustomerDatabase.Domain;
+using System.Web;
+
+namespace SimpleCustomerDatabase.Web.Tests.ModelTests
+{
+    [TestFixture]
+    class CustomerModelTests
+    {
+        private void Setup()
+        {
+            
+        }
+
+        private void Testing_Model()
+        {
+            
+        }
+    }
+}
